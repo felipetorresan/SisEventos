ec2ec3a0b972e0355dbdb11e27831fe6d8ceec56
felipetorresan <felipe.schran@aluno.famper.edu.br>
2024-09-25 20:01:45 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/siseventos/SisEventosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.siseventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/famper/siseventos/SisEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.siseventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
